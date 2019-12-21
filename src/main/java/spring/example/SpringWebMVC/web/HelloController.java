f4525fc7aca0dcf39537ba160154c211453d02a0
Suite <a471486944@gmail.com>
2019-12-21 19:35:28 +0800

    Spring Web MVC 页面重定向示例提交

--- src/main/java/spring/example/SpringWebMVC/web/HelloController.java
+++ src/main/java/spring/example/SpringWebMVC/web/HelloController.java
@@ -2,18 +2,29 @@ package spring.example.SpringWebMVC.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String hello(Model model) {
         model.addAttribute("message", "Hello World!");
         // 备注: 返回值作为视图名称
         return "hello";
+    }
+
+    @RequestMapping(value = "/redirectPage")
+    public String redirectPage() {
+        // URL 重定向
+        return "redirect:/hello/finalPage";
+    }
+
+    @RequestMapping(value = "/finalPage")
+    public String finalPage() {
+        return "final";
     }
 
 }
